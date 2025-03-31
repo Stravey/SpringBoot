100c1830a5f70d083fe97020d71e5c86ce55f1d3
strive <2205487298@qq.com>
2025-03-31 21:19:58 +0800

    update SpringBoot

--- src/main/java/com/liu/Controller/HelloController.java
+++ src/main/java/com/liu/Controller/HelloController.java
@@ -1,13 +1,56 @@
 package com.liu.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private Environment env;
+
+    @Value("${address[0]}")
+    private String address;
+    @RequestMapping("/address")
+    public String getAddress() {
+        return address;
+    }
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${person.name}")
+    private String personName;
+
+    @Value("${person.age}")
+    private int age;
+
+    @RequestMapping("/index")
+    public String index() {
+        System.out.println(personName);
+        System.out.println(age);
+
+        System.out.println(env.getProperty("person.name"));
+        System.out.println(env.getProperty("address[2]"));
+
+        return "Hello World" + " " + personName + " " + age;
+    }
+
+    @RequestMapping("/name")
+    public String name() {
+        System.out.println(name);
+        return "Hello " + name;
+    }
+
     @RequestMapping("/hello")
     public String hello() {
         return "Hello Spring Boot ";
     }
+
+
+
 }
